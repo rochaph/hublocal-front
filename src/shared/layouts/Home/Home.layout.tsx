0f8fe3c51299f43d04d112f31b34678c22e3b341
Pedro Henrique <pdhro99@gmail.com>
2022-01-16 14:41:56 -0400

    fix: altura minima para layout home

--- src/shared/layouts/Home/Home.layout.tsx
+++ src/shared/layouts/Home/Home.layout.tsx
@@ -7,6 +7,7 @@ const StyledMain = styled.main`
   display: flex;
   align-items: center;
   justify-content: center;
+  min-height: 100vh;
 `;
 
 function HomeLayout({ children }: { children: ReactNode }) {
